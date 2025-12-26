6bbe6ae0399ef63c40b9c70f8466995e75010002
emil-devel <emil-devel@gmx.net>
2025-12-26 08:39:36 +0100

    app.css -> layout.css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import '../app.css';
+	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import Header from '$lib/site/Header.svelte';
 	import Footer from '$lib/site/Footer.svelte';
